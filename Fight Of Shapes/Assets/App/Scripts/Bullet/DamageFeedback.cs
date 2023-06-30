1361d645aa968efc9d785b7fda8b6859c84ba5a1
E1sad <106135426+E1sad@users.noreply.github.com>
2023-06-30 11:47:21 +0400

    Version 0.5.2
    some cosmetic visual improvement

--- Fight Of Shapes/Assets/App/Scripts/Bullet/DamageFeedback.cs
+++ Fight Of Shapes/Assets/App/Scripts/Bullet/DamageFeedback.cs
@@ -1,3 +1,4 @@
+using SOG.Enemy;
 using SOG.Game_Manager;
 using System.Collections;
 using System.Collections.Generic;
@@ -12,13 +13,16 @@ namespace SOG.Bullet{
 
     [Header("Links")]
     [SerializeField] private GameObject _textPrefab;
-    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private ParticleSystem[] _particleSystem;
 
     //Internal varibales
     private List<GameObject> _allTextGameObjects;
     private List<GameObject> _sendableTextGameObjects;
     private List<GameObject> _sendedTextGameObjects;
     private int _localTimeScale;
+    private ParticleSystem[][] _particleSystemArray;
+    private int[] _sendableParticleIndex;
+
 
     #region My Methods
     private IEnumerator instantiateTextObjects(){ 
@@ -29,11 +33,30 @@ namespace SOG.Bullet{
         _sendableTextGameObjects.Add(textObject);
         yield return new WaitForSeconds(0.05f);}
     }
+    private IEnumerator instantiateParticleSystems(){
+      _particleSystemArray = new ParticleSystem[_particleSystem.Length][];
+      _sendableParticleIndex = new int[_particleSystem.Length];
+      for (int i = 0; i < _particleSystem.Length; i++){
+        _particleSystemArray[i] = new ParticleSystem[5];
+        _sendableParticleIndex[i] = 0;
+        for (int j = 0; j < 5; j++){
+          ParticleSystem particle = Instantiate(_particleSystem[i], transform);
+          _particleSystemArray[i][j] = particle;
+          yield return new WaitForSeconds(0.05f);}}
+    }
+    private void desTroyedParticleFeedback(Vector3 position, int corner) {
+      int indexOfShape = findIndexOfShapeFromCorner(corner);
+      if (indexOfShape > 4) return;
+      ParticleSystem particle = null;
+      particle = _particleSystemArray[indexOfShape][_sendableParticleIndex[indexOfShape]%5];
+      if (particle == null) return;
+      _sendableParticleIndex[indexOfShape]++;
+      particle.transform.position = position;
+      particle.Play();
+    }
     private IEnumerator damageFeedback(Vector3 position,int damage) {
       float elapsed = 0f;
       GameObject textObject = send();
-/*      _particleSystem.transform.position = position;
-      _particleSystem.Play();*/
       textObject.transform.position = position;
       position += new Vector3(.5f, 1, 0);
       TMP_Text textFeatures = textObject.GetComponent<TextMeshProUGUI>();
@@ -71,13 +94,20 @@ namespace SOG.Bullet{
         case GameState.PLAY_STATE: _localTimeScale = 1; break;
         default: break;}
     }
+    private int findIndexOfShapeFromCorner(int corner) {
+      switch (corner) {
+        case 0: return 0; case 3: return 1; case 4: return 2;
+        case 5: return 3; case 6: return 4; default: return 10;}
+    }
     private void restartState(){
       StopAllCoroutines();
       for (int i = 0; i < _sendedTextGameObjects.Count; i++){
         _sendedTextGameObjects[i].SetActive(false);
         _sendableTextGameObjects.Add(_sendedTextGameObjects[i]);
-        _sendedTextGameObjects.RemoveAt(0);
-      }
+        _sendedTextGameObjects.RemoveAt(0);}
+    }
+    private void destroyEnemyEventHandler(object sender, DestroyEnemyEventArgs evetnArgs){
+      desTroyedParticleFeedback(evetnArgs.ThisEnemy.gameObject.transform.position, evetnArgs.ThisEnemy.Corner);
     }
     #endregion
 
@@ -86,16 +116,20 @@ namespace SOG.Bullet{
       _allTextGameObjects = new List<GameObject>();
       _sendableTextGameObjects = new List<GameObject>();
       _sendedTextGameObjects = new List<GameObject>();
+        
       StartCoroutine(instantiateTextObjects());
+      StartCoroutine(instantiateParticleSystems());
       _localTimeScale = 1;
     }
     private void OnEnable(){
       SpawnDamageFeedbackEvent.EventSpawnDamageFeedback += SpawnDamageFeedbackEventHandler;
       GameStateEvents.OnGameStateChanged += gameStateHandler;
+      DestroyEnemyEvent.EventDestroyEnemy += destroyEnemyEventHandler;
     }
     private void OnDisable(){
       SpawnDamageFeedbackEvent.EventSpawnDamageFeedback -= SpawnDamageFeedbackEventHandler;
       GameStateEvents.OnGameStateChanged -= gameStateHandler;
+      DestroyEnemyEvent.EventDestroyEnemy -= destroyEnemyEventHandler;
     }
     #endregion
   }
--- Fight Of Shapes/Assets/App/Scripts/Player/Shooting.cs
+++ Fight Of Shapes/Assets/App/Scripts/Player/Shooting.cs
@@ -3,6 +3,7 @@ using SOG.Game_Manager;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace SOG.Player
 {
@@ -13,9 +14,11 @@ namespace SOG.Player
 
     [Header("Links")]
     [SerializeField] private Movement _playerMovement;
+    [SerializeField] private Image _reloadImage;
 
     //Internal varibales
-    private Coroutine _routine;
+    private Coroutine _shootingRoutine;
+    private Coroutine _reloadRoutine;
     private bool _isGamePlayState;
 
     #region My Methods
@@ -28,9 +31,19 @@ namespace SOG.Player
     }
     private IEnumerator shootingCoroutine(){
       while (_isGamePlayState){
+        startReloadRoutine();
         yield return new WaitForSeconds(_ShootingFrequency);
         SpawnBulletEvent.Raise(this,
-          new SpawnBulletEventArgs(fromLocationToVector(_playerMovement._playerLocation), Quaternion.identity));}
+          new SpawnBulletEventArgs(fromLocationToVector(_playerMovement._playerLocation), Quaternion.identity));
+        stopReloadRoutine();}
+    }
+    private IEnumerator reloadRoutine() {
+      float elapsed = 0f;
+      _reloadImage.fillAmount = 0;
+      while(elapsed < _ShootingFrequency) {
+        _reloadImage.fillAmount = elapsed / _ShootingFrequency;
+        elapsed += Time.deltaTime;
+        yield return null;}
     }
     private void gameStateHandler(object sender, GameStateChangedEvent eventargs){
       switch (eventargs.CurrentGameState){
@@ -40,13 +53,23 @@ namespace SOG.Player
         case GameState.PLAY_STATE: gamePlayState(); break;
         default: break;}
     }
-    private void restartAndIdleState() {_isGamePlayState = true; stopCoroutine();}
-    private void pauseState() { _isGamePlayState = false; stopCoroutine();}
-    private void gamePlayState() {_isGamePlayState = true; startCoroutine();}
-    private void startCoroutine() { 
-      if (_routine != null) StopCoroutine(_routine); _routine = StartCoroutine(shootingCoroutine());
+    private void restartAndIdleState() {_isGamePlayState = true; stopShootingCoroutine();}
+    private void pauseState() { _isGamePlayState = false; stopShootingCoroutine();}
+    private void gamePlayState() {_isGamePlayState = true; startShootingCoroutine();}
+    private void startShootingCoroutine() { 
+      if (_shootingRoutine != null) StopCoroutine(_shootingRoutine); 
+      _shootingRoutine = StartCoroutine(shootingCoroutine());
     }
-    private void stopCoroutine() { if(_routine != null) StopCoroutine(_routine); _routine = null; }
+    private void stopShootingCoroutine() { 
+      if(_shootingRoutine != null) StopCoroutine(_shootingRoutine); _shootingRoutine = null; 
+    }
+    private void startReloadRoutine() { 
+      if (_reloadRoutine != null) StopCoroutine(_reloadRoutine); 
+      _reloadRoutine = StartCoroutine(reloadRoutine());
+    }
+    private void stopReloadRoutine() { 
+      if (_reloadRoutine != null) StopCoroutine(_reloadRoutine); _reloadRoutine = null;
+    }
     #endregion
 
     #region Unity's Methods
